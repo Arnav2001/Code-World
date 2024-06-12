30d3e5698fa6dd4b87a873bb66e409e445635d0f
Arnav Kulshrestha <arnavkul07@gmail.com>
2024-06-12 15:36:16 +0530

    on boarding UI improved

--- lib/onBoardingScreen.dart
+++ lib/onBoardingScreen.dart
@@ -46,17 +46,17 @@ class OnBoardingPage extends StatelessWidget {
           decoration: getPageDecoration(),
         ),
       ],
-      done: Text('Start', style: TextStyle(fontWeight: FontWeight.w600)),
+      done: Text('Start', style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white)),
       onDone: () {
         goToHome(context);
         SharedPrefs();
         },
       showSkipButton: true,
-      skip: Text('Skip'),
+      skip: Text('Skip',style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white)),
       onSkip: () {goToHome(context);
       SharedPrefs();},
-      next: Icon(Icons.arrow_forward),
-      dotsDecorator: getDotDecoration(),
+      next: Icon(Icons.arrow_forward, color: Colors.white,),
+      dotsDecorator: getDotDecoration( ),
       onChange: (index) => print('Page $index selected'),
       globalBackgroundColor: Theme.of(context).primaryColor,
       nextFlex: 0,
@@ -71,16 +71,18 @@ class OnBoardingPage extends StatelessWidget {
       Center(child: Image.asset(path, width: 350));
 
   DotsDecorator getDotDecoration() => DotsDecorator(
-    color: Color(0xFFBDBDBD),
+    color: Colors.grey,
     size: Size(10, 10),
     activeSize: Size(22, 10),
+    activeColor: Colors.white,
     activeShape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(24),
+
     ),
   );
 
   PageDecoration getPageDecoration() => PageDecoration(
-    titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+    titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold,),
     bodyTextStyle: TextStyle(fontSize: 20),
     bodyPadding: EdgeInsets.all(16).copyWith(bottom: 0),
     imagePadding: EdgeInsets.all(24),
